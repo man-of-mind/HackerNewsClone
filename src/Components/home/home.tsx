b5469e56516c815c3a844cbc59617a41e3c1f2e8
Abiodun Ganiyu <timothyabiodun300@gmail.com>
2022-05-27 00:54:25 +0100

    initial project set up

---
+++ src/Components/home/home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./home.module.scss";
+
+
+const HomePage = () => {
+    return(
+        <div className={styles['home']}>
+            <h1>Hacker News Clone</h1>
+        </div>
+    );
+}
+
+export default HomePage;
